daf861f1a54535ce6563bcb87c850122331c0f01
LAPTOP-BIASGQI5\ttmin <ttminhtrang09@gmail.com>
2017-11-03 18:12:26 +0200

    quick fix

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
 
-var quoteLibrary = [];
+    var quoteLibrary = [];
 
-(function getQuoteLibrary() {
-  $.ajax({
-    url: "https://raw.githubusercontent.com/trangtmtran/Random-Quote-Machine/master/quotes.json",
-    type: 'GET',
-    dataType: 'json',
-    success: function(result){
-         quoteLibrary = result.quotes;
-         console.log(quoteLibrary.length);
-     }});
-})();
+    (function getQuoteLibrary() {
+        $.ajax({
+          url: "https://raw.githubusercontent.com/trangtmtran/Random-Quote-Machine/master/quotes.json",
+          type: 'GET',
+          dataType: 'json',
+          success: function(result){
+             quoteLibrary = result.quotes;
+             console.log(quoteLibrary.length);
+        }});
+    })();
 
 
 
-$("#getQuote").on("click",function() {
-  var ranNum = Math.floor(Math.random() * quoteLibrary.length) + 1;
-    $("#quote").fadeIn("slow").html("&quot;" + quoteLibrary[ranNum]["quote"] + "&quot;");
-        $("#author").html(quoteLibrary[ranNum]["author"]);
-          $("#share-it").attr("href", "https://twitter.com/intent/tweet/?text="
-                                                  + quoteLibrary[ranNum]["quote"]
-                                                  + quoteLibrary[ranNum]["author"]);
- });
+    $("#getQuote").on("click",function() {
+        var ranNum = Math.floor(Math.random() * quoteLibrary.length) + 1;
+            $("#quote").fadeIn("slow").html("&quot;" + quoteLibrary[ranNum]["quote"] + "&quot;");
+                $("#author").html("-" + quoteLibrary[ranNum]["author"]);
+                    $("#share-it").attr("href", "https://twitter.com/intent/tweet/?text="
+                                                      + quoteLibrary[ranNum]["quote"]
+                                                      + quoteLibrary[ranNum]["author"]);
+    });
 });
